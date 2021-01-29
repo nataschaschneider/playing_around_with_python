e14eab7de6ea1e99e21a31d0365faa9aa7cd1599
nataschaschneider <68851658+nataschaschneider@users.noreply.github.com>
2021-01-29 23:28:02 +0000

    Update city_country_river_winner_20210129.py

--- city_country_river_winner_20210129.py
+++ city_country_river_winner_20210129.py
@@ -1,11 +1,14 @@
 # City country river winner - Helps you win every game!
 
+# libraries and modules
 import pandas as pd
+from tabulate import tabulate
 
 # program data
 category_answers = "C:/Users/nschn/Documents/python_code/city_country_river_winner_data_20210129.csv"
 answer_df = pd.read_csv(category_answers)
-
+alphabet = answer_df["Letter"].tolist()
+answer_df.set_index("Letter", drop=True, inplace=True)
 
 # game requirements and input check
 categories = input("Please enter the required categories (from city, country, river, animal, name),\n"
@@ -24,20 +27,20 @@ while not all(x in program_categories for x in category_list):
 else:
     pass
 
+
 # playing the game
 def play():
-    alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "g", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
     letter = input("Please enter the required letter and confirm by pressing ENTER:\n")
-    while not letter.lower() in alphabet:
+    letter = letter.upper()
+    while letter not in alphabet:
         letter = input("That's not right. Please enter the required letter and confirm by pressing ENTER:\n")
+        letter = letter.upper()
         continue
     else:
         pass
-    row_number = alphabet.index(letter.lower())
-
     # retrieving answers
-    winning_answers = answer_df.loc[row_number, category_list]
-    print(winning_answers)
+    winning_answers = answer_df.loc[[letter], category_list]
+    print(tabulate(winning_answers, headers=category_list, tablefmt='fancy_grid'))
 
 
 game = input("\nReady to play (y/n)? Please confirm by pressing ENTER.\n")
